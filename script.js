fba06660aa7885c94f33b6ca031ca9a964ff149e
LuisF-1995 <luis-1995@live.com.ar>
2022-03-28 09:46:54 -0500

    cargue avance generacion plantilla con datos terminada e inicio de desarrollo para guardar en PDF

--- script.js
+++ script.js
@@ -1,92 +1,122 @@
-let datosPersona = {
-    nombre: "",
-    edad: 0,
-    ciudad: "",
-    interesPorJs: "",
-  };
+let datosPersona = [
+  {
+    nombreEstudiante: "Luis Rodriguez",
+    documento: 1093780708,
+    descripcionCurso: "“Diseño y dimensionamiento de la infraestructura de soporte para el cumplimiento del Reglamento para redes internas de telecomunicaciones RITEL de acuerdo con la resolución 5993 de 2020”",
+    duracionCurso: "El curso tuvo una duración de treinta y cinco (35) horas, dictado en Febrero de 2022",
+    firmaCapacitador: "./img/firma.png",
+    nombreCapacitador: "Ing. Marcos Quintana"
+  },
+  {
+    nombreEstudiante: "Angie Andrea Pulido",
+    documento: 1093778168,
+    descripcionCurso: "“Diseño y dimensionamiento de la infraestructura de soporte para el cumplimiento del Reglamento para redes internas de telecomunicaciones RITEL de acuerdo con la resolución 5993 de 2020”",
+    duracionCurso: "El curso tuvo una duración de treinta y cinco (35) horas, dictado en Febrero de 2022",
+    firmaCapacitador: "./img/firma.png",
+    nombreCapacitador: "Ing. Oliver Arenas"
+  },
+  {
+    nombreEstudiante: "Ana Botero",
+    documento: 1093779144,
+    descripcionCurso: "“Diseño y contruccion de instalaciones electricas RETIE/RETILAP”",
+    duracionCurso: "El curso tuvo una duración de treinta y cinco (40) horas, dictado en Febrero de 2022",
+    firmaCapacitador: "./img/firma.png",
+    nombreCapacitador: "Ing. Felipe Andres Henao"
+  },
+  {
+    nombreEstudiante: "Felipe Andres Henao",
+    documento: 1093780708,
+    descripcionCurso: "“REVIT basico y avanzado para modelado 3D, con tecnologias BIM”",
+    duracionCurso: "El curso tuvo una duración de treinta y cinco (60) horas, dictado en Marzo de 2022",
+    firmaCapacitador: "./img/firma.png",
+    nombreCapacitador: "Ing. Luis F. Rodriguez"
+  },
+  {
+    nombreEstudiante: "MARIA RANGEL",
+    documento: 630315248,
+    descripcionCurso: "“Cocina para perritos”",
+    duracionCurso: "El curso tuvo una duración de treinta y cinco (20) horas, dictado en Enero de 2022",
+    firmaCapacitador: "./img/firma.png",
+    nombreCapacitador: "Chef. Ana Botero"
+  }
+];
   
-  const listado = [{
-      imgUrl: "https://huguidugui.files.wordpress.com/2015/03/html1.png",
-      lenguajes: "HTML y CSS",
-      bimestre: "1er bimestre",
-    },
-    {
-      imgUrl: "https://jherax.files.wordpress.com/2018/08/javascript_logo.png",
-      lenguajes: "Javascript",
-      bimestre: "2do bimestre",
-    },
-    {
-      imgUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png",
-      lenguajes: "React JS",
-      bimestre: "3er bimestre",
-    },
-  ];
-  
-  const generarCertificado = document.querySelector("#generarCertificado");
-  const guardarCertificado = document.querySelector("#guardarCertificado");
-  
-  generarCertificado.addEventListener("click", renderizarDatosUsuario);
-  guardarCertificado.addEventListener("click", recorrerListadoYRenderizarTarjetas);
+const generarCertificado = document.querySelector("#generarCertificado");
+const downPdf = document.getElementById("guardarCertificadoPDF");
 
+generarCertificado.addEventListener("click", generacionCertificado);
+downPdf.addEventListener("click", descargarPDF);
+
+function generacionCertificado() {
+
+  let cajaFila = document.querySelector(".certificadoCreado");
+  cajaFila.innerHTML = ``;
   
-  function obtenerDatosDelUsuario() {
-    let fechaActual = new Date();
+  function crearCaja(nombre, documento, descripcionCurso, duracion, firmaCapacitador, nombreCapacitador){
+
+    let cajaFila = document.querySelector(".certificadoCreado");
+    const templateCaja = `<section class="certificado">
+                          <img src="./img/plantillaCertificado.png" alt="Fondo certificado">
+                          <div class="datos">
+                          <p id="nombre"><strong>${nombre}</strong></p>
+                          <p id="cedula">${documento}</p>
+                          <p id="tituloAsistio">Asistió al curso:</p>
+                          <p id="descripcionCurso">${descripcionCurso}</p>
+                          <p id="duracionCurso">${duracion}</p>
+                            <div class="capacitador">
+                                <img src="${firmaCapacitador}" alt="Firma capacitador">
+                                <p id="nombreCapacitador"><strong>${nombreCapacitador}</strong><br>Capacitador</p>
+                            </div>
+                          </div>
+                          </section>`;
     
-    datosPersona.nombre = prompt("Por favor ingrese su nombre");
-    datosPersona.edad = fechaActual.getFullYear() - parseInt(prompt("Ingresa el año en que naciste"));
-    datosPersona.ciudad = prompt("Ingresa la ciudad donde vives");
-    datosPersona.interesPorJs = confirm("¿Te interesa javaScript?");
+    cajaFila.innerHTML += templateCaja;
   }
   
-  function renderizarDatosUsuario() {
-    /* ------------------- NO TOCAR NI ELIMINAR ESTA FUNCION. ------------------- */
-    obtenerDatosDelUsuario();
-    /* --------------- PUNTO 2: Escribe tu codigo a partir de aqui --------------- */
-    let mostrarNombre = document.querySelector("#nombre");
-    mostrarNombre.innerText = `${datosPersona.nombre}`;
-  
-    let mostrarEdad = document.getElementById("edad");
-    mostrarEdad.innerText = `${datosPersona.edad}`;
-  
-    let mostrarCiudad = document.getElementById("ciudad");
-    mostrarCiudad.innerText = `${datosPersona.ciudad}`;
-  
-    let mostrarInteresJs = document.getElementById("javascript");
-    mostrarInteresJs.innerText = `${(datosPersona.interesPorJs) ? "Si" : "No"}`;
-  }
-  
-  
-  function recorrerListadoYRenderizarTarjetas() {
-    /* ------------------ PUNTO 3: Escribe tu codigo desde aqui ------------------ */
-    let cajaFila = document.getElementById("fila");
-    cajaFila.innerHTML = ``;
-    
-    function crearCaja(imagen, lenguajes, bimestre){
-  
-      let cajaFila = document.getElementById("fila");
-      const templateCaja = `<div class = "caja">
-                            <img src="${imagen}" alt="${lenguajes}">
-                            <p class="lenguajes">${lenguajes}</p>
-                            <p class="bimestre">${bimestre}</p>
-                            </div>`;
-      
-      cajaFila.innerHTML += templateCaja;
-    }
-    
-    listado.forEach(elemento => crearCaja(elemento.imgUrl, elemento.lenguajes, elemento.bimestre));
-  
-  }
-  
-  function alternarColorTema() {
-    /* --------------------- PUNTO 4: Escribe tu codigo aqui --------------------- */
-    let pagina = document.getElementById("sitio");
-    pagina.classList.toggle("dark");
-  }
-  
-  /* --------------------- PUNTO 5: Escribe tu codigo aqui --------------------- */
-  let textoOculto = document.getElementById("sobre-mi");
-  window.addEventListener("keypress", function(evento){
-    if ((evento.key).toLowerCase() == "f"){
-      textoOculto.classList.remove("oculto");
-    }
-  })
+  datosPersona.forEach(persona => crearCaja(persona.nombreEstudiante, persona.documento, persona.descripcionCurso, persona.duracionCurso, persona.firmaCapacitador, persona.nombreCapacitador));
+
+}
+
+
+function descargarPDF(){
+      downPdf.onclick = function() {
+          html2canvas(document.body, {
+              onrendered:function(canvas) {
+
+                  var contentWidth = canvas.width;
+                  var contentHeight = canvas.height;
+
+                  //一页pdf显示html页面生成的canvas高度;
+                  var pageHeight = contentWidth / 595.28 * 841.89;
+                  //未生成pdf的html页面高度
+                  var leftHeight = contentHeight;
+                  //pdf页面偏移
+                  var position = 0;
+                  //a4纸的尺寸[595.28,841.89]，html页面生成的canvas在pdf中图片的宽高
+                  var imgWidth = 555.28;
+                  var imgHeight = 555.28/contentWidth * contentHeight;
+
+                  var pageData = canvas.toDataURL('image/jpeg', 1.0);
+
+                  var pdf = new jsPDF('', 'pt', 'a4');
+                  //有两个高度需要区分，一个是html页面的实际高度，和生成pdf的页面高度(841.89)
+                  //当内容未超过pdf一页显示的范围，无需分页
+                  if (leftHeight < pageHeight) {
+                      pdf.addImage(pageData, 'JPEG', 20, 0, imgWidth, imgHeight );
+                  } else {
+                      while(leftHeight > 0) {
+                          pdf.addImage(pageData, 'JPEG', 20, position, imgWidth, imgHeight)
+                          leftHeight -= pageHeight;
+                          position -= 841.89;
+                          //避免添加空白页
+                          if(leftHeight > 0) {
+                              pdf.addPage();
+                          }
+                      }
+                  }
+
+                  pdf.save('content.pdf');
+              }
+          })
+      }
+}
